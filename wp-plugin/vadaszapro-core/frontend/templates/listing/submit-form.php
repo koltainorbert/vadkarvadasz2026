ed1d596aa2fe79d974b0242568817e63a1afd838
koltainorbert <koltainorbert@users.noreply.github.com>
2026-05-14 14:01:55 +0200

    Deploy_2026.05.14_14.01

--- wp-plugin/vadaszapro-core/frontend/templates/listing/submit-form.php
+++ wp-plugin/vadaszapro-core/frontend/templates/listing/submit-form.php
@@ -485,7 +485,7 @@ body.va-page-modal-open{
     align-items: center;
     justify-content: center;
     padding: 11px 14px;
-    margin: 0;
+    margin: 0 0 0 auto;
     background: #ff8a00 !important;
     border: 1px solid #ff8a00 !important;
     border-radius: 30px;
