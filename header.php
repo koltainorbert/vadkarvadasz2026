4eeb39caffde9bf91d27d930c901aafc58c42ae4
koltainorbert <koltainorbert@users.noreply.github.com>
2026-05-26 21:17:47 +0200

    Auto_2026.05.26_21.17

--- header.php
+++ header.php
@@ -46,9 +46,9 @@
 <div class="va-site-wrap<?php echo ! is_front_page() ? ' va-site-wrap--inner' : ''; ?>">
     <?php $auctions_enabled = function_exists( 'va_auctions_enabled' ) ? va_auctions_enabled() : true; ?>
     <?php
-    $brand_name = trim( (string) get_option( 'va_site_name', 'weingartnerauto.hu' ) );
-    if ( in_array( $brand_name, [ '', 'VadászApró', 'Vadaszapro', 'Weingartner Auto', 'WEINGARTNER AUTÓ', 'WEINGARTNER AUTO' ], true ) ) {
-        $brand_name = 'weingartnerauto.hu';
+    $brand_name = trim( (string) get_option( 'va_site_name', 'Vadkár Radar' ) );
+    if ( in_array( $brand_name, [ '', 'VadászApró', 'Vadaszapro', 'Weingartner Auto', 'WEINGARTNER AUTÓ', 'WEINGARTNER AUTO', 'weingartnerauto.hu' ], true ) ) {
+        $brand_name = 'Vadkár Radar';
     }
     $brand_icon  = get_option( 'va_brand_icon_url', '' );
     $header_logo = get_option( 'va_header_logo_url', '' );
